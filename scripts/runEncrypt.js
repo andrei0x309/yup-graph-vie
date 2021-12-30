c05b21eccaeef2b02832e4bcbc5c72a5cd15992d
Andrei O <andrei@flashsoft.eu>
2021-12-30 18:12:12 +0200

    wip: add SPA encryption

--- scripts/runEncrypt.js
+++ scripts/runEncrypt.js
@@ -1,6 +1,14 @@
+import dotenv from 'dotenv';
+import { encrypt } from 'pagecrypt'
+import fs from 'fs'
+ 
+dotenv.config();
 
-
-
+const ENCRYPT_PASS = process.env['DISCORD_TOKEN']
 
 
 console.log("Starting post build encryption of SPA");
+
+await encrypt('dist/index.html', 'dist/encrypted.html', ENCRYPT_PASS)
+fs.unlinkSync('dist/index.html')
+fs.renameSync('dist/encrypted.html', 'dist/index.html' )
