b0e82d7ede202f1b1d2aae59729e14ea9e041d8c
Andrei O <andrei@flashsoft.eu>
2022-01-04 22:22:20 +0200

    fix: replace in encrypt script

--- scripts/runEncrypt.mjs
+++ scripts/runEncrypt.mjs
@@ -14,7 +14,7 @@ await encrypt('dist/index.html', 'dist/encrypted.html', ENCRYPT_PASS)
 fs.unlinkSync('dist/index.html')
 fs.renameSync('dist/encrypted.html', 'dist/index.html' )
 let data = fs.readFileSync('dist/index.html', 'utf8')
-data = data.replace(/<a.*?\/a>/, '').replace('<main class', '<main style="background: linear-gradient(to right, #0f0c29, #302b63, #24243e);" class=')
+data = data.replace(/<a.*?\/a>/, '').replace('<main class', '<main style="background: linear-gradient(to right, #0f0c29, #302b63, #24243e);" class')
 fs.writeFileSync('dist/index.html', data, {
     encoding: 'utf8',
     flag: 'w'
